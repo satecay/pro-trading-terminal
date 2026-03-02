37eccc9595541578958f7767dcbe453d38f75ea1
satecay <satecay@gmail.com>
2026-03-02 11:23:02 +0700

    Update pro.py

--- pro.py
+++ pro.py
@@ -326,9 +326,9 @@ with st.expander("📊 DANH MỤC TRỰC CHIẾN & NHẬT KÝ LỆNH", expanded=
                         elif current_p != last_price:
                             diff = current_p - last_price
                             if diff > 0:
-                                trend_msg = f"📈 HỒI LÊN: +{diff:,.2f} giá (Từ {last_price:,.2f} ➡️ {current_p:,.2f})"
+                                trend_msg = f"📈 HỒI LÊN: +{diff:,.2f} giá (Từ {last_price:,.2f} ➡️ {current_p:,.2f}) GIÁ MUA: {buy_p:,.2f}" # thêm dòng này
                             else:
-                                trend_msg = f"📉 RƠI TIẾP: {diff:,.2f} giá (Từ {last_price:,.2f} ➡️ {current_p:,.2f})"
+                                trend_msg = f"📉 RƠI TIẾP: {diff:,.2f} giá (Từ {last_price:,.2f} ➡️ {current_p:,.2f}) GIÁ MUA: {buy_p:,.2f}"
                             should_send = True
 
                         if should_send:
@@ -586,6 +586,7 @@ else:
         )
         send_telegram_alert(plan_msg)
         st.toast(f"✅ Đã gửi kế hoạch {symbol} vào Telegram của bạn!", icon="🚀")
+
 
 
 
